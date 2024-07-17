8b79baec68ddde5a0c238a53266e177f6823ca9a
bugraOzd <bugraozdilek@gmail.com>
2024-07-17 21:28:44 +0300

    delete unused navbar and imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,9 @@
-import { useState, useEffect } from "react";
-// import { Table } from '@/components/ui/table';
 import "./App.css";
-import Navbar from "./components/Navbar";
 import Quiz from "./components/Quiz";
 
 function App() {
   return (
     <main className="text-center">
-      {/* <Navbar /> */}
       <h1 className="text-primary font-bold py-10">QUIZ-APP</h1>
       <Quiz />
     </main>
